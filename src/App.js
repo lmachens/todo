7acd9f4573b8defa2f6fd1d1c34f1e22e6a8e4e0
lmachens <leon.machens@googlemail.com>
2020-08-19 10:16:39 +0200

    Add Router with two pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import { getTodos } from "./api/todos";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   const [todos, setTodos] = useState(null);
@@ -14,11 +15,21 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
-      {todos?.map((todo) => (
-        <div key={todo.id}>{todo.title}</div>
-      ))}
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/add">
+          <div>ADD TASK</div>
+        </Route>
+        <Route path="/">
+          <div>All Tasks</div>
+        </Route>
+      </Switch>
+      <div className="App">
+        {todos?.map((todo) => (
+          <div key={todo.id}>{todo.title}</div>
+        ))}
+      </div>
+    </Router>
   );
 }
 
