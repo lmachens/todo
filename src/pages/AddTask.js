b6890ff9e44ff23c78acd4d943f8c287ceb387bf
lmachens <leon.machens@googlemail.com>
2020-08-19 12:25:00 +0200

    Add form to create new task

--- src/pages/AddTask.js
+++ src/pages/AddTask.js
@@ -1,11 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 function AddTask() {
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+
+  function handleTitleChange(event) {
+    setTitle(event.target.value);
+  }
+
+  function handleAuthorChange(event) {
+    setAuthor(event.target.value);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    alert("Submitted" + title + " " + author);
+  }
+
   return (
-    <div>
-      <Link to="/">Tasks</Link> ADD TASK
-    </div>
+    <>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Title:
+          <input value={title} onChange={handleTitleChange} />
+        </label>
+        <label>
+          Author:
+          <input value={author} onChange={handleAuthorChange} />
+        </label>
+        <input type="submit" value="Add task" />
+      </form>
+      <Link to="/">Tasks</Link>
+    </>
   );
 }
 
